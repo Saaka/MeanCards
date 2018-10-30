02f5268e595fa6caa16896779e6b839235a88599
Michał Sakwa <saka1988@gmail.com>
2018-10-30 21:51:24 +0100

    Added Game and GameRound data models.

---
+++ MeanCards.Model/DataModels/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeanCards.Model.DataModels
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int OwnerId { get; set; }
+        public bool ShowAdultContent { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ MeanCards.Model/DataModels/GameRound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MeanCards.Model.DataModels
+{
+    public class GameRound
+    {
+        public int GameRoundId { get; set; }
+        public int GameId { get; set; }
+        public int RoundOwnerId { get; set; }
+        public bool IsActive { get; set; }
+        public int? RoundWinnerId { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public Game Game { get; set; }
+    }
+}
