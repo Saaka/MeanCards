5c919d6e458af553b427e40d57a1a5bcfbe957a5
Michał Sakwa <saka1988@gmail.com>
2018-11-07 23:54:36 +0100

    Added tests for card repositories.

---
+++ MeanCards.Tests.Integration/RepositoryTests/AnswerCardRepositoryShould.cs
@@ -0,0 +1,86 @@
+﻿using MeanCards.DAL.Repository;
+using MeanCards.DAL.Storage;
+using MeanCards.Tests.Integration.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MeanCards.Tests.Integration.RepositoryTests
+{
+    public class QuestionCardRepositoryShould
+    {
+        readonly DbContextOptions<AppDbContext> contextOptions;
+        const string DefaultLanguageCode = "PL";
+
+        public QuestionCardRepositoryShould()
+        {
+            contextOptions = TestInMemoryDbOptionsProvider.CreateOptions<AppDbContext>();
+        }
+
+        [Fact]
+        public async Task InsertQuestionCards()
+        {
+            var languageId = await CreateDefaultLanguage();
+
+            await PopulateQuestionCards(languageId);
+
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new QuestionCardsRepository(context);
+
+                var cards = await cardRepository.GetAllActiveQuestionCards();
+
+                Assert.Equal(2, cards.Count);
+            }
+        }
+
+        [Fact]
+        public async Task ReturnCardsWithoutMatureContent()
+        {
+            var languageId = await CreateDefaultLanguage();
+
+            await PopulateQuestionCards(languageId);
+
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new QuestionCardsRepository(context);
+
+                var cards = await cardRepository.GetQuestionCardsWithoutMatureContent();
+
+                Assert.Single(cards);
+            }
+        }
+
+        private async Task PopulateQuestionCards(int languageId)
+        {
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new QuestionCardsRepository(context);
+
+                await cardRepository.CreateQuestionCard(new Model.ViewModel.CreateQuestionCardModel
+                {
+                    IsAdultContent = true,
+                    LanguageId = languageId,
+                    NumberOfAnswers = 1,
+                    Text = "Test1"
+                });
+                await cardRepository.CreateQuestionCard(new Model.ViewModel.CreateQuestionCardModel
+                {
+                    IsAdultContent = false,
+                    LanguageId = languageId,
+                    NumberOfAnswers = 1,
+                    Text = "Test2"
+                });
+            }
+        }
+
+        private async Task<int> CreateDefaultLanguage()
+        {
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var languageRepository = new LanguagesRepository(context);
+                return await languageRepository.CreateLanguage(new Model.ViewModel.CreateLanguageModel { Code = DefaultLanguageCode, Name = "Polski " });
+            }
+        }
+    }
+}
---
+++ MeanCards.Tests.Integration/RepositoryTests/QuestionCardRepositoryShould.cs
@@ -0,0 +1,84 @@
+﻿using MeanCards.DAL.Repository;
+using MeanCards.DAL.Storage;
+using MeanCards.Tests.Integration.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MeanCards.Tests.Integration.RepositoryTests
+{
+    public class AnswerCardRepositoryShould
+    {
+        readonly DbContextOptions<AppDbContext> contextOptions;
+        const string DefaultLanguageCode = "PL";
+
+        public AnswerCardRepositoryShould()
+        {
+            contextOptions = TestInMemoryDbOptionsProvider.CreateOptions<AppDbContext>();
+        }
+
+        [Fact]
+        public async Task InsertAnswerCards()
+        {
+            var languageId = await CreateDefaultLanguage();
+
+            await PopulateAnswerCards(languageId);
+
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new AnswerCardsRepository(context);
+
+                var cards = await cardRepository.GetAllActiveAnswerCards();
+
+                Assert.Equal(2, cards.Count);
+            }
+        }
+
+        [Fact]
+        public async Task ReturnCardsWithoutMatureContent()
+        {
+            var languageId = await CreateDefaultLanguage();
+
+            await PopulateAnswerCards(languageId);
+
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new AnswerCardsRepository(context);
+
+                var cards = await cardRepository.GetAnswerCardsWithoutMatureContent();
+
+                Assert.Single(cards);
+            }
+        }
+
+        private async Task PopulateAnswerCards(int languageId)
+        {
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var cardRepository = new AnswerCardsRepository(context);
+
+                await cardRepository.CreateAnswerCard(new Model.ViewModel.CreateAnswerCardModel
+                {
+                    IsAdultContent = true,
+                    LanguageId = languageId,
+                    Text = "Test1"
+                });
+                await cardRepository.CreateAnswerCard(new Model.ViewModel.CreateAnswerCardModel
+                {
+                    IsAdultContent = false,
+                    LanguageId = languageId,
+                    Text = "Test2"
+                });
+            }
+        }
+
+        private async Task<int> CreateDefaultLanguage()
+        {
+            using (var context = new AppDbContext(contextOptions))
+            {
+                var languageRepository = new LanguagesRepository(context);
+                return await languageRepository.CreateLanguage(new Model.ViewModel.CreateLanguageModel { Code = DefaultLanguageCode, Name = "Polski " });
+            }
+        }
+    }
+}
