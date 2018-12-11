5d96acb259f5d598d2afab104edca0e853a040f0
Michał Sakwa <saka1988@gmail.com>
2018-12-12 00:12:36 +0100

    Added Dapper for potential usage.

---
+++ MeanCards.DAL/Connections/SqlConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using MeanCards.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MeanCards.DAL.Connections
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection();
+        IDbConnection CreateConnection(string connectionString);
+    }
+
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private readonly IDbConnectionConfig dbConnectionConfig;
+
+        public SqlConnectionFactory(IDbConnectionConfig dbConnectionConfig)
+        {
+            this.dbConnectionConfig = dbConnectionConfig;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return CreateConnection(dbConnectionConfig.GetConnectionString());
+        }
+
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ MeanCards.DAL/Repository/Base/BaseSqlRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using MeanCards.DAL.Connections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeanCards.DAL.Repository.Base
+{
+    public class BaseSqlRepository
+    {
+        protected readonly IDbConnectionFactory dbConnectionFactory;
+
+        public BaseSqlRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            this.dbConnectionFactory = dbConnectionFactory;
+        }
+
+        protected virtual T QueryFirstOrDefault<T>(string sql, object parameters = null)
+        {
+            using (var connection = dbConnectionFactory.CreateConnection())
+            {
+                return connection.QueryFirstOrDefault<T>(sql, parameters);
+            }
+        }
+
+        protected virtual List<T> Query<T>(string sql, object parameters = null)
+        {
+            using (var connection = dbConnectionFactory.CreateConnection())
+            {
+                return connection.Query<T>(sql, parameters).ToList();
+            }
+        }
+
+        protected virtual int Execute(string sql, object parameters = null)
+        {
+            using (var connection = dbConnectionFactory.CreateConnection())
+            {
+                return connection.Execute(sql, parameters);
+            }
+        }
+    }
+}
