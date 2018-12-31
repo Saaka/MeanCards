b371cbb4c5c621b2cc80cf14f108a7b2e2359134
Michał Sakwa <saka1988@gmail.com>
2018-12-31 15:46:40 +0100

    Added tests for base game request validator.

--- MeanCards.Tests.Unit/ValidatorTests/BaseTests/BaseGameRequestsValidatorShould.cs
+++ MeanCards.Tests.Unit/ValidatorTests/BaseTests/BaseGameRequestsValidatorShould.cs
@@ -1,15 +1,58 @@
 ﻿using MeanCards.Model.Core;
 using MeanCards.Model.Core.Games.Base;
+using MeanCards.Validators.Games;
+using Moq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MeanCards.Tests.Unit.ValidatorTests.BaseTests
 {
-    public class BaseGameRequestsValidatorShould
+    public class BaseGameRequestsValidatorShould : IClassFixture<BaseGameRequestsValidatorSut>
     {
-        [Fact]
-        public void Test()
+        private readonly BaseGameRequestsValidatorSut validatorSut;
+        public BaseGameRequestsValidatorShould(BaseGameRequestsValidatorSut validatorSut)
         {
+            this.validatorSut = validatorSut;
+        }
 
+        [Fact]
+        public async Task ShouldCallGameRequestValidator()
+        {
+            var request = new BaseRequestConcrete();
+
+            await validatorSut.Validator.Validate(request);
+
+            validatorSut.GameRequestValidatorMock.Verify(x => x.Validate(request));
+        }
+
+        [Fact]
+        public async Task ShouldCallGameRoundRequestValidator()
+        {
+            var request = new BaseRequestConcrete();
+
+            await validatorSut.Validator.Validate(request);
+
+            validatorSut.GameRoundRequestValidatorMock.Verify(x => x.Validate(request));
+        }
+
+        [Fact]
+        public async Task ShouldCallUserRequestValidator()
+        {
+            var request = new BaseRequestConcrete();
+
+            await validatorSut.Validator.Validate(request);
+
+            validatorSut.UserRequestValidatorMock.Verify(x => x.Validate(request));
+        }
+
+        [Fact]
+        public async Task ShouldCallPlayerRequestValidator()
+        {
+            var request = new BaseRequestConcrete();
+
+            await validatorSut.Validator.Validate(request);
+
+            validatorSut.PlayerRequestValidatorMock.Verify(x => x.Validate(request));
         }
 
         private class BaseRequestConcrete : IBaseRequest, IGameRequest, IGameRoundRequest, IUserRequest, IPlayerRequest
---
+++ MeanCards.Tests.Unit/ValidatorTests/BaseTests/BaseGameRequestsValidatorSut.cs
@@ -0,0 +1,85 @@
+﻿using MeanCards.Model.Core;
+using MeanCards.Model.Core.Games.Base;
+using MeanCards.Validators;
+using MeanCards.Validators.Games;
+using Moq;
+using System.Threading.Tasks;
+
+namespace MeanCards.Tests.Unit.ValidatorTests.BaseTests
+{
+    public class BaseGameRequestsValidatorSut
+    {
+        public Mock<IRequestValidator<IGameRequest>> GameRequestValidatorMock { get; private set; }
+        public Mock<IRequestValidator<IGameRoundRequest>> GameRoundRequestValidatorMock { get; private set; }
+        public Mock<IRequestValidator<IUserRequest>> UserRequestValidatorMock { get; private set; }
+        public Mock<IRequestValidator<IPlayerRequest>> PlayerRequestValidatorMock { get; private set; }
+
+        public BaseGameRequestsValidator Validator { get; private set; }
+
+        public BaseGameRequestsValidatorSut()
+        {
+            GameRequestValidatorMock = CreateGameRequestValidator();
+            GameRoundRequestValidatorMock = CreateGameRoundRequestValidator();
+            UserRequestValidatorMock = CreateUserRequestValidator();
+            PlayerRequestValidatorMock = CreatePlayerRequestValidator();
+
+            Validator = new BaseGameRequestsValidator(
+                GameRequestValidatorMock.Object,
+                GameRoundRequestValidatorMock.Object,
+                UserRequestValidatorMock.Object,
+                PlayerRequestValidatorMock.Object);
+        }
+
+        private Mock<IRequestValidator<IPlayerRequest>> CreatePlayerRequestValidator()
+        {
+            var mock = new Mock<IRequestValidator<IPlayerRequest>>();
+            mock.Setup(x => x.Validate(It.IsAny<IPlayerRequest>()))
+                .Returns(() =>
+                {
+                    return Task.FromResult(new ValidatorResult());
+                })
+                .Verifiable();
+
+            return mock;
+        }
+
+        private Mock<IRequestValidator<IUserRequest>> CreateUserRequestValidator()
+        {
+            var mock = new Mock<IRequestValidator<IUserRequest>>();
+            mock.Setup(x => x.Validate(It.IsAny<IUserRequest>()))
+                .Returns(() =>
+                {
+                    return Task.FromResult(new ValidatorResult());
+                })
+                .Verifiable();
+
+            return mock;
+        }
+
+        private Mock<IRequestValidator<IGameRoundRequest>> CreateGameRoundRequestValidator()
+        {
+            var mock = new Mock<IRequestValidator<IGameRoundRequest>>();
+            mock.Setup(x => x.Validate(It.IsAny<IGameRoundRequest>()))
+                .Returns(() =>
+                {
+                    return Task.FromResult(new ValidatorResult());
+                })
+                .Verifiable();
+
+            return mock;
+        }
+
+        private Mock<IRequestValidator<IGameRequest>> CreateGameRequestValidator()
+        {
+            var mock = new Mock<IRequestValidator<IGameRequest>>();
+            mock.Setup(x => x.Validate(It.IsAny<IGameRequest>()))
+                .Returns(() =>
+                {
+                    return Task.FromResult(new ValidatorResult());
+                })
+                .Verifiable();
+
+            return mock;
+        }
+    }
+}
