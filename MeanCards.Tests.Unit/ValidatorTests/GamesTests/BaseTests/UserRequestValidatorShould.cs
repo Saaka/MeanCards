3c252d15a5f3c19c6082cde943b2e09144548678
Michał Sakwa <saka1988@gmail.com>
2019-01-02 00:11:10 +0100

    Added tests for user request validator.

---
+++ MeanCards.Tests.Unit/ValidatorTests/GamesTests/BaseTests/UserRequestValidatorShould.cs
@@ -0,0 +1,81 @@
+﻿using MeanCards.Common.Constants;
+using MeanCards.DAL.Interfaces.Repository;
+using MeanCards.Model.Core;
+using MeanCards.Validators.Games.Base;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MeanCards.Tests.Unit.ValidatorTests.GamesTests.BaseTests
+{
+    public class UserRequestValidatorShould
+    {
+        [Fact]
+        public async Task ReturnSuccessResultForValidData()
+        {
+            var playerRepo = CreateUsersRepoMock();
+            var validator = new UserRequestValidator(playerRepo);
+
+            var request = new UserRequestConcrete
+            {
+                UserId = 1
+            };
+
+            var result = await validator.Validate(request);
+
+            Assert.True(result.IsSuccessful);
+            Assert.Null(result.Error);
+        }
+
+        [Fact]
+        public async Task ReturnFailureResultForMissingUserId()
+        {
+            {
+                var playerRepo = CreateUsersRepoMock();
+                var validator = new UserRequestValidator(playerRepo);
+
+                var request = new UserRequestConcrete
+                {
+                };
+
+                var result = await validator.Validate(request);
+
+                Assert.False(result.IsSuccessful);
+                Assert.Equal(ValidatorErrors.Games.UserIdRequired, result.Error);
+            }
+        }
+
+        [Fact]
+        public async Task ReturnFailureResultForMissingOrInactiveUser()
+        {
+            {
+                var playerRepo = CreateUsersRepoMock(
+                    userExists: false);
+                var validator = new UserRequestValidator(playerRepo);
+
+                var request = new UserRequestConcrete
+                {
+                    UserId = 1
+                };
+
+                var result = await validator.Validate(request);
+
+                Assert.False(result.IsSuccessful);
+                Assert.Equal(ValidatorErrors.Users.UserIdNotFound, result.Error);
+            }
+        }
+
+        private IUsersRepository CreateUsersRepoMock(bool userExists = true)
+        {
+            var mock = new Mock<IUsersRepository>();
+            mock.Setup(m => m.ActiveUserExists(It.IsAny<int>())).Returns(Task.FromResult<bool>(userExists));
+
+            return mock.Object;
+        }
+
+        class UserRequestConcrete : IUserRequest
+        {
+            public int UserId { get; set; }
+        }
+    }
+}
--- MeanCards.Tests.Unit/ValidatorTests/GamesTests/CreateGameValidatorShould.cs
+++ MeanCards.Tests.Unit/ValidatorTests/GamesTests/CreateGameValidatorShould.cs
@@ -68,13 +68,5 @@ namespace MeanCards.Tests.Unit.ValidatorTests.GamesTests
             Assert.False(result.IsSuccessful);
             Assert.Equal(ValidatorErrors.Games.GameNameRequired, result.Error);
         }
-
-        private IUsersRepository CreateMock(bool userExists = true)
-        {
-            var mock = new Mock<IUsersRepository>();
-            mock.Setup(m => m.ActiveUserExists(It.IsAny<int>())).Returns(Task.FromResult<bool>(userExists));
-
-            return mock.Object;
-        }
     }
 }
