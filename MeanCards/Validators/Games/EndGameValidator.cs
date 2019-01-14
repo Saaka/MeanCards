b361d022648c05875cd436c9ec8fee9c8b949217
Michał Sakwa <saka1988@gmail.com>
2019-01-15 00:15:22 +0100

    Added end game validator.

--- MeanCards/Validators/Games/EndGameValidator.cs
+++ MeanCards/Validators/Games/EndGameValidator.cs
@@ -1,4 +1,7 @@
-﻿using MeanCards.Model.Core.Games;
+﻿using MeanCards.Common.Constants;
+using MeanCards.DAL.Interfaces.Repository;
+using MeanCards.Model.Core.Games;
+using MeanCards.Validators.Games.ValidationRules;
 using System;
 using System.Threading.Tasks;
 
@@ -6,9 +9,35 @@ namespace MeanCards.Validators.Games
 {
     public class EndGameValidator : IRequestValidator<EndGame>
     {
+        private readonly IBaseGameRequestsValidator baseGameRequestsValidator;
+        private readonly IGameOwnerRule gameOwnerRule;
+        private readonly IGamesRepository gamesRepository;
+
+        public EndGameValidator(
+            IBaseGameRequestsValidator baseGameRequestsValidator,
+            IGameOwnerRule gameOwnerRule,
+            IGamesRepository gamesRepository)
+        {
+            this.baseGameRequestsValidator = baseGameRequestsValidator;
+            this.gameOwnerRule = gameOwnerRule;
+            this.gamesRepository = gamesRepository;
+        }
+
         public async Task<ValidatorResult> Validate(EndGame request)
         {
-            throw new NotImplementedException();
+            var baseResult = await baseGameRequestsValidator.Validate(request);
+            if (!baseResult.IsSuccessful)
+                return new ValidatorResult(baseResult.Error);
+
+            var isOwnerResult = await gameOwnerRule.Validate(request);
+            if (!isOwnerResult.IsSuccessful)
+                return new ValidatorResult(isOwnerResult.Error);
+
+            var gameStatus = await gamesRepository.GetGameStatus(request.GameId);
+            if (gameStatus != Common.Enums.GameStatusEnum.InProgress)
+                return new ValidatorResult(ValidatorErrors.Games.GameNotFoundOrInactive);
+
+            return new ValidatorResult();
         }
     }
 }
---
+++ MeanCards/Validators/Games/ValidationRules/GameOwnerRule.cs
@@ -0,0 +1,35 @@
+﻿using MeanCards.Common.Constants;
+using MeanCards.DAL.Interfaces.Repository;
+using MeanCards.Model.Core;
+using MeanCards.Model.Core.Games.Base;
+using System.Threading.Tasks;
+
+namespace MeanCards.Validators.Games.ValidationRules
+{
+    public interface IGameOwnerRule
+    {
+        Task<ValidatorResult> Validate<T>(T request)
+            where T : IGameRequest, IUserRequest;
+    }
+
+    public class GameOwnerRule : IGameOwnerRule
+    {
+        private readonly IGamesRepository gamesRepository;
+
+        public GameOwnerRule(
+            IGamesRepository gamesRepository)
+        {
+            this.gamesRepository = gamesRepository;
+        }
+
+        public async Task<ValidatorResult> Validate<T>(T request) 
+            where T : IGameRequest, IUserRequest
+        {
+            var game = await gamesRepository.GetGameById(request.GameId);
+            if (game.OwnerId != request.UserId)
+                return new ValidatorResult(ValidatorErrors.Games.InvalidUserAction);
+
+            return new ValidatorResult();
+        }
+    }
+}
