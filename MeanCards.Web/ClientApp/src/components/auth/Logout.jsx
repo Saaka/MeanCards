386a14b0219190c145e3f67a138a1b58d44bb15c
Michał Sakwa <saka1988@gmail.com>
2019-01-31 00:08:16 +0100

    Changed Logout from function to react component.

--- MeanCards.Web/ClientApp/src/components/auth/Logout.jsx
+++ MeanCards.Web/ClientApp/src/components/auth/Logout.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { AuthService } from '../services/Services';
 
-export function Logout(props) {
-    const authService = new AuthService();
+export class Logout extends Component {
+    authService = new AuthService();
 
-    authService.logout();
-    props.history.replace('/login');
+    componentWillMount() {
+        this.authService.logout();
+        this.props.history.replace('/login');
+    };
 
-    return (
-        <div className="container">
+    render() {
+        return (<div className="container">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <h1>Logging out</h1>
                 </div>
             </div>
         </div>
-    );
+        );
+    };
 };
