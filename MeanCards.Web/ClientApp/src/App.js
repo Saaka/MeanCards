c88ee6e5e12e3925e6084a09b8e0642aae370c17
Michał Sakwa <saka1988@gmail.com>
2019-02-08 07:33:47 +0100

    Change import statement to fix tests in travis.

--- MeanCards.Web/ClientApp/src/App.js
+++ MeanCards.Web/ClientApp/src/App.js
@@ -4,7 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Countdown } from './components/Countdown';
 import { Counter } from './components/Counter';
-import { Login } from './components/auth/AuthComponents';
+import { Login } from './components/auth/Login';
 import { Logout } from './components/auth/Logout';
 
 
